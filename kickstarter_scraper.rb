3ae3bd511c2ba75b6bece93b2291ab9131365bf4
Crystal Norman <cnorm88@gmail.com>
2020-10-16 16:30:40 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -2,13 +2,6 @@
 require 'nokogiri'
 require 'pry'
 
-
-def create_project_hash
-  html = File.read('fixtures/kickstarter.html')
- 
-  kickstarter = Nokogiri::HTML(html)
-  binding.pry
-  
   # projects: kickstarter.css("li.project.grid_4")
   # title: project.css("h2.bbcard_name strong a").text
   # image link: project.css("div.project-thumbnail a img").attribute("src").value
@@ -16,6 +9,11 @@ def create_project_hash
   # location: project.css("span.location-name").text
   # location: project.css("ul.project-meta span.location-name").text
   # percent_funded: project.css("ul.project-stats li.first.funded strong").text.gsub("%","").to_i
+
+def create_project_hash
+  html = File.read('fixtures/kickstarter.html')
+ 
+  kickstarter = Nokogiri::HTML(html)
 end
 
 create_project_hash
